dfccbaee93274171ae9343409ed4c5eaace4e108
A1esha <72608100+A1esha@users.noreply.github.com>
2022-07-07 12:32:07 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,18 +1,14 @@
 <?php
-  $phone = filter_var(trim($_POST['phone']),
-  FILTER_SANITIZE_STRING);
 
-  $name = filter_var(trim($_POST['name']),
-  FILTER_SANITIZE_STRING);
+  $phone = filter_var(trim($_POST['phone']),FILTER_SANITIZE_STRING);
 
-  $email = filter_var(trim($_POST['email']),
-  FILTER_SANITIZE_STRING);
+  $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
 
-  if(mb_string($name)<2)
-  {
-    echo "Введите своё имя корректно";
-    exit();
-  }
-  echo "Привет";
-  exit();
+  $email = filter_var(trim($_POST['email']),FILTER_SANITIZE_STRING);
+
+  $mysql = new mysqli('localhost','root','root','students');
+
+  $mysql->query("INSERT INTO `users` (`phone`,`name`,`email`) VALUES('$phone','$name','$email')");
+
+  $mysql->close();
  ?>
