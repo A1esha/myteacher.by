fe3dd99363f1b0f68456d02d99393262e0b559d3
A1esha <72608100+A1esha@users.noreply.github.com>
2022-07-13 17:12:47 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,17 +1,26 @@
 <?php
 
-  $phone = filter_var(trim($_POST['phone']),FILTER_SANITIZE_STRING);
 
-  $name = filter_var(trim($_POST['name']),FILTER_SANITIZE_STRING);
 
-  $email = filter_var(trim($_POST['email']),FILTER_SANITIZE_STRING);
 
+  $phone = $_POST['phone'];
+
+  $name =  $_POST['name'];
+
+  $email =  $_POST['email'];
+
+  $teacher =  $_POST['teacher'];
+  if(empty($teacher))
+  {
+    $teacher = "NONE";
+  }
   $mysql = new mysqli('localhost','root','root','students');
 
-  $mysql->query("INSERT INTO `users` (`phone`,`name`,`email`) VALUES('$phone','$name','$email')");
+	$mysql->query("INSERT INTO `users` (`phone`,`name`,`email`,`teacher`) VALUES('$phone','$name','$email','$teacher')");
 
   $mysql->close();
 
-  header('Location: /');
+  #header('Location: /');
 
- ?>
+  #exit('<meta http-equiv = "refresh" content = "0; url = index.html" />');
+?>
